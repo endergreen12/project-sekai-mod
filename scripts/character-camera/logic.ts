28f3ac249dcd7b8535cc1fa477b99df76fc5ce96
endergreen12 <endergreenjapan@gmail.com>
2025-07-27 23:47:12 +0900

    三人称視点を実装

--- scripts/character-camera/logic.ts
+++ scripts/character-camera/logic.ts
@@ -1,18 +1,29 @@
 import "frida-il2cpp-bridge"
 
+const enableThirdPerson = false
 export function AttachCameraToCharacterModel(camera: Il2Cpp.Object, characterModel: Il2Cpp.Object)
 {
     const cameraTransform = camera.method<Il2Cpp.Object>("get_transform").invoke()
-    cameraTransform.method("SetParent", 1).invoke(characterModel.method<Il2Cpp.Object>("get_HeadTransform").invoke())
+    cameraTransform.method("SetParent", 1).invoke(characterModel.method<Il2Cpp.Object>(`get_${enableThirdPerson ? "PositionNote" : "HeadTransform"}`).invoke())
 
     const Vector3Class = Il2Cpp.domain.assembly("UnityEngine.CoreModule").image.class("UnityEngine.Vector3")
     const localPosition = Vector3Class.alloc()
-    localPosition.method(".ctor", 3).invoke(-0.05, 0.0, 0.0)
     const localEulerAngles = Vector3Class.alloc()
-    localEulerAngles.method(".ctor", 3).invoke(0.0, 0.0, 90.0) // おそらくHeadTransformが90度傾いているのでカメラも逆に傾ける必要がある
-
+    if(enableThirdPerson)
+    {
+        localPosition.method(".ctor", 3).invoke(0.0, 0.6, 1.5)
+        localEulerAngles.method(".ctor", 3).invoke(180.0, 0.0, 180.0)
+    } else {
+        localPosition.method(".ctor", 3).invoke(-0.05, 0.0, 0.0)
+        localEulerAngles.method(".ctor", 3).invoke(0.0, 0.0, 90.0)
+    }
     cameraTransform.method("set_localPosition").invoke(localPosition.unbox())
     cameraTransform.method("set_localEulerAngles").invoke(localEulerAngles.unbox())
+
+    if(enableThirdPerson)
+    {
+        return
+    }
 
     characterModel.method<Il2Cpp.Object>("get_Face").invoke().method("SetActive").invoke(false)
     characterModel.method<Il2Cpp.Object>("get_Hair").invoke().method("SetActive").invoke(false)
@@ -24,6 +35,6 @@ export function ChangeFOV()
 
     assembly.class("Sekai.Core.SekaiCameraAspect").method("CalculateVerticalFov").implementation = function(currentFov: number)
     {
-        return 90 // オーバーライドするFOVの値(100を超えたあたりからキャラクターのアウトラインがめっちゃ太くなるので注意)
+        return 70 // オーバーライドするFOVの値(100を超えたあたりからキャラクターのアウトラインがめっちゃ太くなるので注意)
     }
 }
